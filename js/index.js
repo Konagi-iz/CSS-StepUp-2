69437b61304614cf51143e5feb9d1d92f404b095
Konagi-iz <cmaxwsl@gmail.com>
2023-12-14 10:35:17 +0900

    js

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// hamburger menu
-$(".menu").on("click", hamToggle);
-
-$(".nav-list__link").on("click", hamToggle);
-
-function hamToggle() {
-	$(".menu").toggleClass("menu--open");
-	$(".nav").toggleClass("nav--active");
-}
